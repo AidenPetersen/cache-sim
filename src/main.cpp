3e77e67d905d56847224494c40d321e10f338faf
Aiden Petersen <aidenpetersen@protonmail.com>
2025-03-04 14:21:16 -0600

    Switched to meson

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
 #include "memqueue.hpp"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-    std::string input_trace_path(argv[1]);
-    MemQueue mq(input_trace_path);
+  std::string input_trace_path(argv[1]);
+  MemQueue mq(input_trace_path);
 }
